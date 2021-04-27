80be9369c0f255ef47680cbccb4a29d12accb49d
Regis Santos <regis42santos@gmail.com>
2021-04-27 10:49:04 -0300

    Creating our item Resource

--- code/app.py
+++ code/app.py
@@ -6,12 +6,26 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Student(Resource):
+items = []
+
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {
+            'name': name,
+            'price': 12.00
+        }
+        items.append(item)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 
 
 app.run(port=5000)
