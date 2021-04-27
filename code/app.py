27d9d4898824a905dd85e0699c540fc91f201017
Regis Santos <regis42santos@gmail.com>
2021-04-27 11:34:05 -0300

    DELETE to delete Items

--- code/app.py
+++ code/app.py
@@ -33,6 +33,11 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
 
 class ItemList(Resource):
     def get(self):
