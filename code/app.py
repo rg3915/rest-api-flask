0a7c2212eecf145f5a76619627a65e7179c4335c
Regis Santos <regis42santos@gmail.com>
2021-04-27 11:54:19 -0300

    Optimising our final code and request parsing

--- code/app.py
+++ code/app.py
@@ -14,6 +14,14 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank."
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -23,7 +31,8 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f"An item with name {name} already exists."}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = {
             'name': name,
             'price': data['price']
@@ -32,14 +41,8 @@ class Item(Resource):
         return item, 201
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'price',
-            type=float,
-            required=True,
-            help="This field cannot be left blank."
-        )
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {
