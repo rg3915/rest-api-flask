7e08d632befefa6db5c54662c3da6036232eab6a
Regis Santos <regis42santos@gmail.com>
2021-04-27 10:56:20 -0300

    The ItemList and creating Items

--- code/app.py
+++ code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -17,15 +17,22 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self, name):
+        data = request.get_json()
         item = {
             'name': name,
-            'price': 12.00
+            'price': data['price']
         }
         items.append(item)
         return item, 201
 
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
 
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
