1818c7ddcde8b1faf4c8614b88afb7bbe5f39e10
Regis Santos <regis42santos@gmail.com>
2021-04-27 11:03:09 -0300

    Improving code and error control

--- code/app.py
+++ code/app.py
@@ -11,12 +11,13 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f"An item with name {name} already exists."}, 400
+
         data = request.get_json()
         item = {
             'name': name,
