b85edac086015f2617feec97830b795bf6d0dedc
Regis Santos <regis42santos@gmail.com>
2021-04-28 15:41:27 -0300

    Logging in and retrieving Users from a database

--- section5/code/security.py
+++ section5/code/security.py
@@ -2,12 +2,12 @@ from user import User
 from werkzeug.security import safe_str_cmp
 
 
-def authenticate(username, password):  # here
+def authenticate(username, password):
     user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
-def identity(payload):  # here
+def identity(payload):
     user_id = payload['identity']
     return User.find_by_id(user_id)
