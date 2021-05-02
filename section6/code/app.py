ca9c4177425e7170f0a63f86aead1e677199736e
Regis Santos <regis42santos@gmail.com>
2021-05-01 22:01:21 -0300

    Telling SQLAlchemy about our tables and columns

--- section6/code/app.py
+++ section6/code/app.py
@@ -7,6 +7,9 @@ from resources.user import UserRegister
 from security import authenticate, identity
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = config('SECRET_KEY')
 api = Api(app)
 
@@ -19,6 +22,7 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
+    # Evita circular import.
     from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
--- section6/code/models/item.py
+++ section6/code/models/item.py
@@ -1,44 +1,32 @@
-import sqlite3
+from db import db
 
 
-class ItemModel:
+class ItemModel(db.Model):
+    __tablename__ = 'items'
 
-    def __init__(self, name, price):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Float(precision=2))
+
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+    store = db.relationship('StoreModel')
+
+    def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
+        self.store_id = store_id
 
     def json(self):
         return {'name': self.name, 'price': self.price}
 
     @classmethod
     def find_by_name(cls, name):
-        connection = sqlite3.connect('db.sqlite3')
-        cursor = connection.cursor()
+        return cls.query.filter_by(name=name).first()
 
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-        if row:
-            return cls(row[1], row[2])
-
-    def insert(self):
-        connection = sqlite3.connect('db.sqlite3')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (NULL, ?, ?)"
-        cursor.execute(query, (self.name, self.price))
-
-        connection.commit()
-        connection.close()
-
-    def update(self):
-        connection = sqlite3.connect('db.sqlite3')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (self.price, self.name))
-
-        connection.commit()
-        connection.close()
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
