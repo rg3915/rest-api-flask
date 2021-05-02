5a8067e986c9b1afff680e1e2759ce2da2518ee0
Regis Santos <regis42santos@gmail.com>
2021-05-01 22:51:05 -0300

    Creating a new model: StoreModel

--- section6/code/resources/item.py
+++ section6/code/resources/item.py
@@ -11,6 +11,12 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank."
     )
+    parser.add_argument(
+        'store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store_id."
+    )
 
     @jwt_required()
     def get(self, name):
@@ -25,7 +31,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
             item.save_to_db()
